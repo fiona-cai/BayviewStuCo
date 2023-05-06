c12ee1357200db395c9e8111db9ad8f7a947d7aa
Fiona Cai <69484076+fiona-cai@users.noreply.github.com>
2023-05-06 13:43:27 -0400

    Add files via upload

---
+++ assets/assets/js/articles.js
@@ -0,0 +1,63 @@
+var dataset = [
+    {
+        title: 'World Autism Awareness Day',
+        date: "2023-04-03",
+        time: "All day",
+        description: "",
+    },
+    {
+        title: 'School Play - Hecuba',
+        date: "2023-04-01",
+        time: "7pm-10pm",
+        description: "",
+    },
+    {
+        title: 'Math Contest - Euclid',
+        date: "2023-04-04",
+        time: "7:15am-10am",
+        description: "",
+    },
+    {
+        title: 'Fryer, Galois and Hypatia',
+        date: "2023-04-05",
+        time: "8:30am-10am",
+        description: "Math Contests in the Cafeteria",
+    },
+    {
+        title: 'Music Showcase',
+        date: "2023-04-11",
+        time: "7:00pm-9:30pm",
+        description: "Richmond Hill Centre for the Performing Arts",
+    },
+    {
+        title: 'Under the Spotlight',
+        date: "2023-04-27",
+        time: "7:00pm-9pm",
+        description: "",
+    },
+    {
+        title: 'DECA ICDC',
+        date: "2023-04-20",
+        time: "Week-long event",
+        description: "",
+    },
+    {
+        title: 'Graduation 2023',
+        date: "2023-06-26",
+        time: "All day",
+        description: "",
+    },
+    {
+        title: 'Grade 12 Day',
+        date: "2023-04-28",
+        time: "9am-3pm",
+        description: "",
+    },
+    {
+        title: 'Relay For Life',
+        date: "2023-06-01",
+        time: "10am-7pm",
+        description: "",
+    },
+    
+];
